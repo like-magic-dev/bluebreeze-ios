7de9dffc7e0a530376b581f86cac5e77d0912257
Alessandro Mulloni <ale@mulloni.tech>
2024-12-17 16:54:24 +0100

    Create example app

---
+++ BlueBreezeTest/BlueBreezeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueBreezeTestApp.swift
+//  BlueBreezeTest
+//
+//  Created by Alessandro Mulloni on 17.12.24.
+//
+
+import SwiftUI
+
+@main
+struct BlueBreezeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
